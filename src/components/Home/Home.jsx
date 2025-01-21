964531955f8055068ea1d085b891cdd463aa4653
tandukaruniqur <uniquetandukar143@gmail.com>
2025-01-21 23:04:32 +0545

    buttons added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,15 +3,10 @@ import "./Home.css";
 import logo_header from "../../assets/Assets/logo_header.png";
 import { AiOutlineBars } from "react-icons/ai";
 import Header from "../Header/Header";
-import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { FaCar } from "react-icons/fa";
 
 const Home = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <>
       <div className="body-overlay">
@@ -19,13 +14,31 @@ const Home = () => {
           <div className="container">
             <div className="logo">
               <img src={logo_header} />
-              <AiOutlineBars onClick={toggleMenu} className="menu-icon" />
+              <AiOutlineBars />
             </div>
-            <div className={`header-items ${isMenuOpen ? "show-menu" : ""}`}>
+            <div className="header-items">
               <a href="">Home</a>
               <a href="">Blog</a>
               <a href="">Contact</a>
               <Header />
+            </div>
+          </div>
+        </div>
+        <div className="home">
+          <div className="home-container">
+            <h1>Ride Your Way, Anytime, Anywhere</h1>
+            <div className="buttons">
+              <button className="btn">
+                {" "}
+                <FaUser />
+                Download rider app
+              </button>
+              <button
+                className="btn"
+                style={{ color: "black", backgroundColor: "lightgreen" }}
+              >
+                <FaCar /> Download driver app
+              </button>
             </div>
           </div>
         </div>
